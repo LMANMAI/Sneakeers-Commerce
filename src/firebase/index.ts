996722c864e896c9859ddc3e90037a51dbe275c7
Lucas Maidana <lucas_sknr.010@live.com>
2021-10-26 20:43:09 -0300

    cambios en las env

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyCNzcNcLWl7PmHSiFb9D5j3jxaoDnXfb_c",
-  authDomain: "sneakers-commerce.firebaseapp.com",
-  projectId: "sneakers-commerce",
-  storageBucket: "sneakers-commerce.appspot.com",
-  messagingSenderId: "547006705448",
-  appId: "1:547006705448:web:42cb9a5cf7d977b0d0aa1d",
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
